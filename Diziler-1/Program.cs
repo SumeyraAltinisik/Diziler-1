3af754f39441586bcf859c9a73651da425b4208d
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-22 15:19:25 +0300

    Add project files.

---
+++ Diziler-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] sehirler = new string[4,2] { { "İstanbul", "Türkiye" }, { "Nice", "Fransa" }, {"Milano", "İtalya" }, { "Prag", "Çekya" } };
+        }
+    }
+}
